b96cdcc8e75568beefdc5daf70b340193d2226dc
Sarvan Gill <sarvangill13@gmail.com>
2024-02-04 16:30:23 -0800

    Added a sprite that can be moved around the screen and have some modifications done to it. Used Tonc's obj_demo as a template and have added my own sprite. Was having some trouble with Usenti sprite editor so I just created the sprite directly by changing the hex in Cube.c. Currently the sprite is just a 16px16p cube this might be too big but I will use this for now.

---
+++ source/Cube.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Cube)
+
+//======================================================================
+//
+//	Cube, 8x8@8, 
+//	+ palette 256 entries, not compressed
+//	+ 1 tiles not compressed
+//	Total size: 512 + 64 = 576
+//
+//	Time-stamp: 2024-02-04, 14:52:25
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CUBE_H
+#define GRIT_CUBE_H
+
+#define CubeTilesLen 128
+extern const unsigned short CubeTiles[64];
+
+#define CubePalLen 512
+extern const unsigned short CubePal[256];
+
+#endif // GRIT_CUBE_H
+
+//}}BLOCK(Cube)
